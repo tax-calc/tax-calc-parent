563d46af3598d91f0ebe65298b4f8d00ad694912
lmnguni <lerato.learning@gmail.com>
2020-01-12 18:21:52 +0200

    initial commit

---
+++ src/main/java/com/lmnguni/tax_calc/parent/App.java
@@ -0,0 +1,14 @@
+package com.lmnguni.tax_calc.parent;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
